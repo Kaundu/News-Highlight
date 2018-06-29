06ac6462368adc223fa99f58cb580a689fc162cc
neville <kilonzoneville@gmail.com>
2018-06-29 08:11:43 +0300

    add the index html view method

--- app/main/views.py
+++ app/main/views.py
@@ -8,6 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    political_articles = get_updates('politics')
+    print(political_articles)
+    business_articles = get_updates('business')
+    # print(business_articles)
+    tech = get_updates('technology')
+    entertainment = get_updates('entertainment')
+    title = 'News- Updates'
+    return render_template('index.html', title=title,politics=political_articles, business=business_articles, technology=tech, entertainment=entertainment)
 
     title = 'Home'
 
