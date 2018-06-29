2263c5c41efb871c459a34eb0f5a423d8af555c1
neville <kilonzoneville@gmail.com>
2018-06-29 08:14:54 +0300

    add the API request

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
@@ -38,5 +41,5 @@ class DevConfig(Config):
 config_options = {
     'development': DevConfig,
     'production': ProdConfig,
-    'test': TestConfig
+    # 'test': TestConfig
 }
